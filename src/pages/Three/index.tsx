b17d917bf2eef865bfc5ef653ab1d45f25f3c0fb
zhengliang.zhang <zhengliang.zhang@united-imaging.com>
2021-11-25 09:12:42 +0800

    变更文件结构

--- src/pages/Three/index.tsx
+++ src/pages/Three/index.tsx
@@ -1,5 +1,5 @@
 import { Component } from 'react';
-import Three from '@/components/ThreeCT';
+import Three from '@/pages/Three/ThreeCT';
 import './index.less';
 export default class ThreePage extends Component {
   render() {
